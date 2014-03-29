723e9f88ac794882b81b81067327d9368ba484fe
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-03-29 15:48:34 +1300

    Opens any matching files in src/classes or src/pages

--- SalesforceGoTo.py
+++ SalesforceGoTo.py
@@ -1,26 +1,38 @@
-import sublime, sublime_plugin, re
+import sublime, sublime_plugin, re, os, os.path
 
 class SalesforceGoToCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view = self.view
-        viewSize = self.view.size()
-        print("View size is " + str(viewSize))
-        fullRegion = sublime.Region(0, viewSize)
-        lineRegions = self.view.lines(fullRegion)
-        print("View contains " + str(len(lineRegions)) + " lines")
-        # regex = "\sit\(['\"]+(.*)['\"], function\("
-        # patt = re.compile(regex)
+        if len(self.view.sel()) == 1:
+            word = self.view.substr(self.view.word(self.view.sel()[0]))
+            matchingFilePath = self.getFilePath(word) 
+            if matchingFilePath != None:
+                self.view.window().open_file(matchingFilePath)
+            else:
+                print("Error: cannot find file that matches name '" + word + "'")
+        else:
+            print("Error: No word highlighted")
 
-        # self.specNameArr = []
-        # self.specRegionArr = []
+    # Get full path of given file name. E.g. if given "Home" search for
+    # "Home.cls" or "Home.page" in subdirectories of the "src" directory
+    def getFilePath(self, name):
+        openFolders = self.view.window().folders()
+        if len(openFolders) == 1:
+            baseDir = os.path.abspath(openFolders[0])
+            srcDirPath = os.path.join(baseDir, "src")
+            if os.path.isdir(srcDirPath):
+                subdirDict = dict({
+                    "classes": "cls", 
+                    "pages": "page"
+                })
+                for subdir in os.listdir(srcDirPath):
+                    fullPath = os.path.join(srcDirPath, subdir)
+                    if os.path.isdir(fullPath) and subdir in subdirDict:
+                        fileNameToSearchFor = os.path.join(fullPath, name + "." + subdirDict[subdir])
+                        print("Searching for " + fileNameToSearchFor)
+                        if(os.path.lexists(fileNameToSearchFor)):
+                            return fileNameToSearchFor
+            else:
+                print("Error: Current folder doesn't appear to be a MavensMate directory (failed to find 'src' subdirectory)")
+        else:
+            print("Error: More than one folder currently open")
 
-        # for region in lineRegions :
-        #     lineContents = self.view.substr(region)
-        #     matcher = patt.match(lineContents)
-        #     if matcher is not None :
-        #         specName = matcher.group(1)
-        #         self.specNameArr.append(specName)
-        #         self.specRegionArr.append(region)
-        #         print("Found spec name: " + specName)
-
-        # self.view.window().show_quick_panel(self.specNameArr, self.showSpec
